6761ed92acdc8a2f3d31278e6d6897d93e163ad4
Rafael <rafaelgs0496@gmail.com>
2017-03-28 22:29:18 -0300

    v5.0.1 Added compatibility to Endless Scrolling.

--- Features/VisibleAttachedImages.v5.0.1.js
+++ Features/VisibleAttachedImages.v5.0.1.js
@@ -1,6 +1,6 @@
-function showVAIImages() {
+function showVAIImages(Context) {
     var Matches, I, N;
-    Matches = document.getElementsByClassName(ESGST.SG ? "comment__toggle-attached" : "view_attached");
+    Matches = Context.getElementsByClassName(ESGST.SG ? "comment__toggle-attached" : "view_attached");
     for (I = 0, N = Matches.length; I < N; ++I) {
         Matches[I].nextElementSibling.firstElementChild.classList.remove("is-hidden", "is_hidden");
     }
